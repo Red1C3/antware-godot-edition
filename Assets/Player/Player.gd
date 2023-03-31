5900f899ad6eb4277e53eade94db28c78ae81ecb
Mohammad Issawi <2000issawi@gmail.com>
2023-03-31 08:08:49 +0300

    Implement player 4 directional movement

--- Assets/Player/Player.gd
+++ Assets/Player/Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody
 
+const SPEED=10
+
 var anim_player:AnimationPlayer
 
 var gravity=ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -13,6 +15,21 @@ func _ready():
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y-=gravity
+	else:
+		
+		var direction=Vector3.ZERO
+		if Input.is_action_pressed("player_fwd"):
+			direction+=Vector3.FORWARD
+		if Input.is_action_pressed("player_bck"):
+			direction+=Vector3.BACK
+		if Input.is_action_pressed("player_left"):
+			direction+=Vector3.LEFT
+		if Input.is_action_pressed("player_right"):
+			direction+=Vector3.RIGHT
+		direction=to_global(direction)-transform.origin
+		direction*=-1
+		velocity=direction.normalized()*SPEED
+		
 	velocity=move_and_slide(velocity,Vector3.UP)
 
 func set_vars():
